f49a6429d18888db7e36cb40037b31c14328dac2
ZequnSong <36482357+ZequnSong@users.noreply.github.com>
2018-10-19 01:54:18 -0400

    Create 0043MultiplyStrings.java

---
+++ 0043MultiplyStrings.java
@@ -0,0 +1,30 @@
+/*
+Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+
+Example 1:
+Input: num1 = "2", num2 = "3"
+Output: "6"
+
+Example 2:
+Input: num1 = "123", num2 = "456"
+Output: "56088"
+
+Note:
+The length of both num1 and num2 is < 110.
+Both num1 and num2 contain only digits 0-9.
+Both num1 and num2 do not contain any leading zero, except the number 0 itself.
+You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+
+求两个字符串数字的相乘，输入的两个数和返回的数都是以字符串格式存储
+本题这样做的目的可能是这样可以计算超大数相乘，可以不受int或long的数值范围的约束，
+
+
+
+*/
+class Solution {
+    public String multiply(String num1, String num2) {
+    
+        
+    }
+}
